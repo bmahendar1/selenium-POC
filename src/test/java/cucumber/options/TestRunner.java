6eacd95a5bbb30ea15cce8985e4b50838fbb4611
Beeravalli Mahendar Reddy <bmahendar916190@gmail.com>
2025-01-07 08:31:42 +0530

    commented tags

--- src/test/java/cucumber/options/TestRunner.java
+++ src/test/java/cucumber/options/TestRunner.java
@@ -15,7 +15,7 @@ import io.cucumber.testng.CucumberOptions;
 				"junit:target/cucumber-reports/Cucumber.xml",
 				"json:target/jsonReports/cucumber-report.json"
 			},
-		tags = "@herokuapp and @dynamic_controls_checkbox", //^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
+//		tags = "@herokuapp and @dynamic_controls_checkbox", //^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
 		dryRun = false,
 		monochrome = true//UalFSzJZP4VNVEvRhkwdus3P4IRVQTDf
 	)
